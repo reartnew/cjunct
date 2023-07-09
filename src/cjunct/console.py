e4b64149f454ac7bfff8beb5c489d57d97d70a48
Artem Novikov <reartnew@yandex-team.ru>
2023-07-09 09:55:33 +0200

    CLIReporter

--- src/cjunct/console.py
+++ src/cjunct/console.py
@@ -1,6 +1,7 @@
 """Command-line interface entry"""
 
 import sys
+import typing as t
 
 import classlogging
 import click
@@ -10,8 +11,20 @@ from cjunct.config.constants import C
 from cjunct.exceptions import BaseError
 
 
-class Logger(classlogging.LoggerMixin):
-    """Top-level CLI logger"""
+class CLIReporter(classlogging.LoggerMixin):
+    """Top-level CLI message printer"""
+
+    @classmethod
+    def on_error(cls, exception: Exception) -> t.NoReturn:
+        """Process an error"""
+        log_msg, print_msg, code = (
+            ("BaseError", str(exception), exception.CODE)
+            if isinstance(exception, BaseError)
+            else ("Unhandled exception", f"UNHANDLED EXCEPTION: {exception}", 1)
+        )  # type: t.Tuple[str, str, int]
+        cls.logger.debug(log_msg, exc_info=True)
+        sys.stderr.write(f"! {print_msg.splitlines()[0]}\n")
+        sys.exit(code)
 
 
 @click.group
@@ -25,10 +38,5 @@ def run() -> None:
     classlogging.configure_logging(level=C.LOG_LEVEL)
     try:
         cjunct.Runner().run_sync()
-    except BaseError as e:
-        Logger.logger.debug("BaseError", exc_info=True)
-        print(e)
-        sys.exit(e.CODE)
-    except Exception:
-        Logger.logger.exception()
-        raise
+    except Exception as e:
+        CLIReporter.on_error(e)
--- src/cjunct/exceptions.py
+++ src/cjunct/exceptions.py
@@ -13,30 +13,30 @@ __all__ = [
 class BaseError(Exception):
     """Common base to catch in CLI"""
 
-    CODE: int = 1
+    CODE: int = 101
 
 
 class LoadError(BaseError):
     """Loader regular exception during load process"""
 
-    CODE: int = 2
+    CODE: int = 102
 
     def __init__(self, message: str, stack: t.List[str]) -> None:
         self.message: str = message
         self.stack: t.List[str] = stack
         text: str = message
         if stack:
-            text += f"\nCurrent stack: {' -> '.join(stack)}"
+            text += f"\n  Current config stack: {' -> '.join(stack)}"
         super().__init__(text)
 
 
 class IntegrityError(BaseError):
     """Action net structure error"""
 
-    CODE: int = 3
+    CODE: int = 103
 
 
 class SourceError(BaseError):
     """Source file not recognized"""
 
-    CODE: int = 4
+    CODE: int = 104
