0ea654c5258bfd634f6754ece178ebc6875feefd
Artem Novikov <reartnew@yandex-team.ru>
2023-08-31 17:15:37 +0200

    Reduce complexity: remove useless class

--- src/cjunct/console.py
+++ src/cjunct/console.py
@@ -1,7 +1,6 @@
 """Command-line interface entry"""
 
 import sys
-import typing as t
 from pathlib import Path
 
 import classlogging
@@ -12,21 +11,7 @@ import cjunct
 from cjunct.config.constants import C
 from cjunct.exceptions import BaseError
 
-
-class CLIReporter(classlogging.LoggerMixin):
-    """Top-level CLI message printer"""
-
-    @classmethod
-    def on_error(cls, exception: Exception) -> t.NoReturn:
-        """Process an error"""
-        log_msg, print_msg, code = (
-            ("BaseError", str(exception), exception.CODE)
-            if isinstance(exception, BaseError)
-            else ("Unhandled exception", f"UNHANDLED EXCEPTION: {exception}", 1)
-        )  # type: t.Tuple[str, str, int]
-        cls.logger.debug(log_msg, exc_info=True)
-        sys.stderr.write(f"! {print_msg.splitlines()[0]}\n")
-        sys.exit(code)
+logger = classlogging.get_module_logger()
 
 
 @click.group
@@ -41,8 +26,14 @@ def run() -> None:
     dotenv_loaded: bool = dotenv.load_dotenv(dotenv_path=dotenv_path)
     classlogging.configure_logging(level=C.LOG_LEVEL)
     if dotenv_loaded:
-        CLIReporter.logger.info(f"Loaded environment variables from {dotenv_path!r}")
+        logger.info(f"Loaded environment variables from {dotenv_path!r}")
     try:
         cjunct.Runner().run_sync()
+    except BaseError as e:
+        logger.exception()
+        sys.stderr.write(f"! {e}\n")
+        sys.exit(e.CODE)
     except Exception as e:
-        CLIReporter.on_error(e)
+        logger.exception()
+        sys.stderr.write(f"! UNHANDLED EXCEPTION: {e}\n")
+        sys.exit(1)
