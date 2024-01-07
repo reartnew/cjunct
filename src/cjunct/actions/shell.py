29b476e59610aba1f8762afb0adc8951f0dc5e64
Artem Novikov <reartnew@nebius.com>
2024-01-07 23:15:25 +0100

    Shell returns dicts of strings

--- src/cjunct/actions/shell.py
+++ src/cjunct/actions/shell.py
@@ -1,9 +1,11 @@
 """Separate module for shell-related action"""
 
 import asyncio
+import base64
+import re
 import textwrap
 import typing as t
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from async_shell import Shell
 
@@ -17,21 +19,43 @@ class ShellAction(ActionBase):
 
     command: str = ""
     script: str = ""
+    result: t.Dict[str, str] = field(default_factory=dict, init=False, repr=False)
+
+    YIELD_SCANNER_PATTERN: t.ClassVar[t.Pattern] = re.compile(r"^(.*?)##cjunct\[yield\s*(\S+)\s+(\S*)\s*]##$")
     YIELD_FUNCTION_BOILERPLATE: t.ClassVar[str] = textwrap.dedent(
         """
             yield(){
-              [ "$1" = "" ] && echo "Missing key (first argument)" && exit 1
-              command -v base64 >/dev/null || ( echo "Missing command: base64" && exit 2 )
+              [ "$1" = "" ] && echo "Missing key (first argument)" && return 1
+              command -v base64 >/dev/null || ( echo "Missing command: base64" && return 2 )
               [ "$2" = "" ] && value="$(cat /dev/stdin)" || value="$2"
-              echo "##cjunct[yield $(printf "$1" | base64) $(printf "$value" | base64)]"
-              exit 0
+              echo "##cjunct[yield $(printf "$1" | base64) $(printf "$value" | base64)]##"
+              return 0
             }
             export -f yield"""
     ).lstrip()
 
     async def _read_stdout(self, shell_process: Shell):
+        # Store data prior to the system message
+        memorized_prefix: str = ""
         async for line in shell_process.read_stdout():
-            self.emit(line)
+            # `endswith` is a cheaper check than re.findall
+            if line.endswith("]##") and (matches := self.YIELD_SCANNER_PATTERN.findall(line)):
+                try:
+                    for preceding_content, encoded_key, encoded_value in matches:
+                        memorized_prefix += preceding_content
+                        self.logger.debug(f"Shell action {self.name!r} reported a key: {encoded_key!r}")
+                        key: str = base64.b64decode(encoded_key).decode()
+                        value: str = base64.b64decode(encoded_value).decode()
+                        self.result[key] = value
+                except Exception:
+                    self.logger.warning("Failed while parsing system message", exc_info=True)
+            else:
+                self.emit(memorized_prefix + line)
+                memorized_prefix = ""
+
+        # Do not forget to report system message prefix, if any
+        if memorized_prefix:
+            self.emit(memorized_prefix)
 
     async def _read_stderr(self, shell_process: Shell):
         async for line in shell_process.read_stderr():
@@ -43,7 +67,7 @@ class ShellAction(ActionBase):
             command = f"{self.YIELD_FUNCTION_BOILERPLATE}\n{command}"
         return command
 
-    async def run(self) -> None:
+    async def run(self) -> t.Dict[str, str]:
         async with Shell(command=self._make_command()) as shell_process:
             tasks: t.List[asyncio.Task] = [
                 asyncio.create_task(self._read_stdout(shell_process)),
@@ -51,3 +75,4 @@ class ShellAction(ActionBase):
             ]
             await asyncio.gather(*tasks)
             await shell_process.validate()
+        return self.result
