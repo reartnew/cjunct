3a8e04839edd91a691777ad6712a354cab4e4dd9
Artem Novikov <reartnew@nebius.com>
2024-03-17 13:54:49 +0100

    Split classes

--- src/cjunct/actions/shell.py
+++ src/cjunct/actions/shell.py
@@ -11,6 +11,55 @@ from async_shell import Shell, ShellResult
 from .base import ActionBase, Stderr, ArgsBase, StringTemplate
 from ..config.constants import C
 
+__all__ = [
+    "YIELD_FUNCTION_BOILERPLATE",
+    "StreamScannerActionBase",
+    "ShellArgs",
+    "ShellAction",
+]
+
+YIELD_FUNCTION_BOILERPLATE: str = textwrap.dedent(
+    """
+        yield_outcome(){
+          [ "$1" = "" ] && echo "Missing key (first argument)" && return 1
+          command -v base64 >/dev/null || ( echo "Missing command: base64" && return 2 )
+          [ "$2" = "" ] && value="$(cat /dev/stdin)" || value="$2"
+          echo "##cjunct[yield-outcome-b64 $(printf "$1" | base64) $(printf "$value" | base64)]##"
+          return 0
+        }
+    """
+).lstrip()
+
+
+# pylint: disable=abstract-method
+class StreamScannerActionBase(ActionBase):
+    """Base class for stream-scanning actions"""
+
+    _YIELD_SCAN_PATTERN: t.ClassVar[t.Pattern] = re.compile(r"^(.*?)##cjunct\[yield-outcome-b64\s*(\S+)\s+(\S*)\s*]##$")
+
+    async def _process_system_messages_in_stream(self, stream: t.AsyncIterable[str]) -> t.AsyncGenerator[str, None]:
+        """Extract system messages from a stream"""
+        # Store data prior to the system message
+        memorized_prefix: str = ""
+        async for line in stream:
+            # `endswith` is a cheaper check than re.findall
+            if line.endswith("]##") and (matches := self._YIELD_SCAN_PATTERN.findall(line)):
+                try:
+                    for preceding_content, encoded_key, encoded_value in matches:
+                        memorized_prefix += preceding_content
+                        self.logger.debug(f"Action {self.name!r} stream reported a key: {encoded_key!r}")
+                        key: str = base64.b64decode(encoded_key, validate=True).decode()
+                        value: str = base64.b64decode(encoded_value, validate=True).decode()
+                        self.yield_outcome(key, value)
+                except Exception:
+                    self.logger.warning("Failed while parsing system message", exc_info=True)
+            else:
+                yield memorized_prefix + line
+                memorized_prefix = ""
+        # Do not forget to report system message prefix, if any
+        if memorized_prefix:
+            yield memorized_prefix
+
 
 class ShellArgs(ArgsBase):
     """Args for shell-related actions"""
@@ -27,55 +76,23 @@ class ShellArgs(ArgsBase):
             raise ValueError("Both command and file specified")
 
 
-class ShellAction(ActionBase):
+class ShellAction(StreamScannerActionBase):
     """Shell commands handler"""
 
     args: ShellArgs
 
-    YIELD_SCAN_PATTERN: t.ClassVar[t.Pattern] = re.compile(r"^(.*?)##cjunct\[yield-outcome-b64\s*(\S+)\s+(\S*)\s*]##$")
-    YIELD_FUNCTION_BOILERPLATE: t.ClassVar[str] = textwrap.dedent(
-        """
-            yield_outcome(){
-              [ "$1" = "" ] && echo "Missing key (first argument)" && return 1
-              command -v base64 >/dev/null || ( echo "Missing command: base64" && return 2 )
-              [ "$2" = "" ] && value="$(cat /dev/stdin)" || value="$2"
-              echo "##cjunct[yield-outcome-b64 $(printf "$1" | base64) $(printf "$value" | base64)]##"
-              return 0
-            }
-        """
-    ).lstrip()
+    async def _read_stdout(self, shell_process: Shell) -> None:
+        async for line in self._process_system_messages_in_stream(shell_process.read_stdout()):
+            self.emit(line)
 
-    async def _read_stdout(self, shell_process: Shell):
-        # Store data prior to the system message
-        memorized_prefix: str = ""
-        async for line in shell_process.read_stdout():
-            # `endswith` is a cheaper check than re.findall
-            if line.endswith("]##") and (matches := self.YIELD_SCAN_PATTERN.findall(line)):
-                try:
-                    for preceding_content, encoded_key, encoded_value in matches:
-                        memorized_prefix += preceding_content
-                        self.logger.debug(f"Shell action {self.name!r} reported a key: {encoded_key!r}")
-                        key: str = base64.b64decode(encoded_key, validate=True).decode()
-                        value: str = base64.b64decode(encoded_value, validate=True).decode()
-                        self.yield_outcome(key, value)
-                except Exception:
-                    self.logger.warning("Failed while parsing system message", exc_info=True)
-            else:
-                self.emit(memorized_prefix + line)
-                memorized_prefix = ""
-
-        # Do not forget to report system message prefix, if any
-        if memorized_prefix:
-            self.emit(memorized_prefix)
-
-    async def _read_stderr(self, shell_process: Shell):
+    async def _read_stderr(self, shell_process: Shell) -> None:
         async for line in shell_process.read_stderr():
             self.emit(Stderr(line))
 
     async def _create_shell(self) -> Shell:
         command: str = self.args.command or f"source '{self.args.file}'"
         if C.SHELL_INJECT_YIELD_FUNCTION:
-            command = f"{self.YIELD_FUNCTION_BOILERPLATE}\n{command}"
+            command = f"{YIELD_FUNCTION_BOILERPLATE}\n{command}"
         return Shell(
             command=command,
             environment=self.args.environment,  # type: ignore[arg-type]
