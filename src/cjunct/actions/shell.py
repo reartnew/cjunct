d81604c73dd7add3e4536c8d998ca544d9f011f6
Artem Novikov <reartnew@nebius.com>
2024-04-07 12:14:53 +0200

    Remove shell compatibility shim

--- src/cjunct/actions/shell.py
+++
@@ -1,8 +0,0 @@
-"""Backwards-compatibility shim. Will be removed in next major version."""
-
-from .bundled.shell import ShellAction, ShellArgs
-
-__all__ = [
-    "ShellAction",
-    "ShellArgs",
-]
