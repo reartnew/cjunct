ed0067172d84767a47f16d73f5d7847c34ae9fd5
Artem Novikov <reartnew@nebius.com>
2024-08-05 14:42:23 +0200

    Display types only during rendering failures

--- src/cjunct/runner.py
+++ src/cjunct/runner.py
@@ -230,12 +230,41 @@ class Runner(classlogging.LoggerMixin):
         )
 
         rendered_args_dict: dict = templar.recursive_render(self.loader.get_original_args_dict_for_action(action))
-        parsed_args: ArgsBase = dacite.from_dict(
-            data_class=type(action.args),
-            data=rendered_args_dict,
-            config=dacite.Config(
-                strict=True,
-                cast=[Enum, Path],
-            ),
-        )
+        try:
+            parsed_args: ArgsBase = dacite.from_dict(
+                data_class=type(action.args),
+                data=rendered_args_dict,
+                config=dacite.Config(
+                    strict=True,
+                    cast=[Enum, Path],
+                ),
+            )
+        except dacite.WrongTypeError as e:
+            raise ActionRenderError(
+                f"Unrecognized {e.field_path!r} content type: {self._repr_type(e.value)}"
+                f" (expected {e.field_type!r})"
+            )
         action.args = parsed_args
+
+    @classmethod
+    def _repr_type(cls, data: t.Any) -> str:
+        if isinstance(data, t.Mapping):
+            if not data:
+                return "typing.Dict"
+            return f"typing.Dict[{cls._repr_union(data)}, {cls._repr_union(data.values())}]"
+        if isinstance(data, t.List):
+            if not data:
+                return "typing.List"
+            return f"typing.List[{cls._repr_union(data)}]"
+        return data.__class__.__name__
+
+    @classmethod
+    def _repr_union(cls, collection: t.Iterable) -> str:
+        sorted_unique_types_names: t.List[str] = sorted({cls._repr_type(item) for item in collection})
+        if len(sorted_unique_types_names) == 1:
+            return sorted_unique_types_names[0]
+        none_type_name: str = type(None).__name__
+        if len(sorted_unique_types_names) == 2 and none_type_name in sorted_unique_types_names:
+            non_none_types = [v for v in sorted_unique_types_names if v != none_type_name]
+            return f"typing.Optional[{non_none_types[0]}]"
+        return f"typing.Union[{', '.join(sorted_unique_types_names)}]"
