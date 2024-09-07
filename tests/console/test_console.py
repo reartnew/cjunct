b5d81b7287c2b300416501ad915310de7c0d8887
Artem Novikov <reartnew@nebius.com>
2024-09-07 20:50:37 +0200

    Add param tests

--- tests/console/test_console.py
+++ tests/console/test_console.py
@@ -1,10 +1,21 @@
+# pylint: disable=redefined-outer-name
+"""CLI tests"""
+
+import typing as t
+
 import classlogging
+import pytest
 from click.testing import CliRunner
 from dotenv.main import DotEnv
-import typing as t
 
 from cjunct import console, version
 from cjunct.config.environment import Env
+
+
+class RunnerType(t.Protocol):
+    """Protocol for the `runner` fixture return type"""
+
+    def __call__(self, *args: str, global_flags: t.Optional[t.List[str]] = None) -> t.List[str]: ...
 
 
 def _invoke(*args, **kwargs) -> t.List[str]:
@@ -13,22 +24,61 @@ def _invoke(*args, **kwargs) -> t.List[str]:
     return result.stdout.splitlines()
 
 
-def _noop(*args, **kwargs) -> None:
+def _noop(*args, **kwargs) -> None:  # pylint: disable=unused-argument
     return None
 
 
-def test_cli_version():
+def test_cli_version() -> None:
+    """Check version command"""
     assert _invoke(console.version) == [version.__version__]
 
 
-def test_cli_env_vars():
-    assert _invoke(console.env_vars) == Env.__doc__.splitlines()
+def test_cli_env_vars() -> None:
+    """Check env vars command"""
+    doc: str = t.cast(str, Env.__doc__)
+    assert _invoke(console.env_vars) == doc.splitlines()
 
 
-def test_cli_run(monkeypatch):
+@pytest.fixture
+def runner(monkeypatch: pytest.MonkeyPatch) -> RunnerType:
+    """Setup test runner fed from stdin"""
+    wf_text: str = """---
+    actions:
+      - type: echo
+        message: foo
+    """
     monkeypatch.setattr(DotEnv, "set_as_environment_variables", _noop)
     monkeypatch.setattr(classlogging, "configure_logging", _noop)
-    assert _invoke(console.run, ["-"], input="actions:\n- type: echo\n  message: foo") == [
+
+    def run(*args: str, global_flags: t.Optional[t.List[str]] = None) -> t.List[str]:
+        return _invoke(console.main, (global_flags or []) + ["run", "-"] + list(args), input=wf_text)
+
+    return run
+
+
+def test_cli_run(runner: RunnerType) -> None:
+    """Default run"""
+    assert runner() == [
+        "[echo-0]  | foo",
+        "===============",
+        "SUCCESS: echo-0",
+    ]
+
+
+def test_cli_run_display(runner: RunnerType) -> None:
+    """Run with overridden display"""
+    assert runner(global_flags=["--display", "headers"]) == [
+        " ┌─[echo-0]",
+        " │ foo",
+        " ╵",
+        " ✓ SUCCESS: echo-0",
+    ]
+
+
+@pytest.mark.parametrize("strategy", ["free", "sequential", "loose", "strict", "strict-sequential"])
+def test_cli_run_explicit_strategy(runner: RunnerType, strategy: str) -> None:
+    """Run with overridden strategy"""
+    assert runner("--strategy", strategy) == [
         "[echo-0]  | foo",
         "===============",
         "SUCCESS: echo-0",
